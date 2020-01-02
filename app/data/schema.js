30169d22e0c153d935af4823eeae1048c422593d
Fabricio <Fabricio@SAONB002593.hildebra.intra>
2020-01-02 17:32:45 -0300

    Mutation do GraphQL

--- app/data/schema.js
+++ app/data/schema.js
@@ -1,25 +1,34 @@
 "use strict";
 
+"use strict";
+
 const { makeExecutableSchema } = require("graphql-tools");
 const resolvers = require("./resolvers");
 
+// Define our schema using the GraphQL schema language
 const typeDefs = `
-  type User {
-    id: Int!
-    username: String!
-    password: String!
-    email: String!
-  }
+    type User {
+      id: Int!
+      username: String!
+      password: String
+      email: String!
+    }
+    type Post {
+      id: Int!
+      title: String!
+      slug: String!
+      content: String!
+      user: User!
+    }
 
-  type Query {
-    allUser: [User]
-    fetchUser(id: Int!): User
-  }
-
-  type Mutation {
-    login (email: String!, password: String!): String
-    createUser (username: String!, email: String!, password: String!): User
-  }
+    type Query {
+      allUsers: [User]
+      fetchUser(id: Int!): User
+    }
+    type Mutation {
+      login (email: String!, password: String!): String
+      createUser (username: String!, email: String!, password: String!): User
+    }
 `;
 
 module.exports = makeExecutableSchema({ typeDefs, resolvers });
